eff37e9e3804460e8ff0a1a5916a8cb1eb7aeaca
iliya-baghjari <iliyabaghjari@gmail.com>
2026-07-30 17:57:17 +0330

    updating packages

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-using GameStore.Api.Dtos;
+
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+
+var conString = "Data Source=GameStore.db";
+builder.Services.AddDbContext<GameStoreContext>(options =>
+    options.UseSqlite(conString));
 
 builder.Services.AddValidation();
+
+var app = builder.Build();
 app.MapGamesEndpoints();
 
 app.Run();
+
+public partial class Program;
