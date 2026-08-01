3f84d84f8978522734069e399a832347335d9841
iliya-baghjari <iliyabaghjari@gmail.com>
2026-08-01 23:45:08 +0330

    commenting

--- Program.cs
+++ Program.cs
@@ -3,16 +3,21 @@ using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 using Microsoft.EntityFrameworkCore;
 
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 var conString = "Data Source=GameStore.db";
 builder.Services.AddDbContext<GameStoreContext>(options =>
     options.UseSqlite(conString));
 
+
 builder.Services.AddValidation();
+
 
 var app = builder.Build();
 app.MapGamesEndpoints();
+
 
 app.Run();
 
